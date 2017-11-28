0afaba75a9031af8db057b7ed173b99d2a2226de
phlummox <phlummox@localhost>
2017-11-28 06:13:31 +0000

    allow galgebra to be instilled by non-root users, if they have a user site-packages directory.

--- galgebra/setgapth.py
+++ galgebra/setgapth.py
@@ -9,17 +9,21 @@ if Ga.pth already exists the old version will be deleted.
 """
 
 import os, sys, site
+from os.path import isdir
 
 cur_dir = os.path.abspath('.')
 
+if site.ENABLE_USER_SITE and isdir(site.getusersitepackages()):
+    dist_pkgs = site.getusersitepackages()
+else:
+    dist_pkgs = site.getsitepackages()[0]
+
 if os.name == 'posix' or os.name == 'mac':
-    dist_pkgs = site.getsitepackages()[-1]
     pth_name = dist_pkgs + '/Ga.pth'
     if os.path.exists(pth_name):
         os.system('rm -f ' + pth_name)
 
 if os.name == 'nt':
-    dist_pkgs = site.getsitepackages()[-1]
     dist_lst = dist_pkgs.split('\\')
     pth_name = dist_lst[0] + '\\' + dist_lst[1] + '\\Ga.pth'
     if os.path.exists(pth_name):
